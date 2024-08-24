d0559a596d1945325c8ba877d98af67eaff113cb
Imrane Lawani <naimlawani01@gmail.com>
2024-08-25 00:59:25 +0200

    fix: navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
-import { useAuth } from '../hooks/authContext';
-
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { useAuth } from '../hooks/authContext'; // Assurez-vous que vous avez cette fonction dans votre contexte
 
 const Navbar = () => {
+  const { user, logout } = useAuth(); // Utiliser le contexte d'authentification
 
-  const { user } = useAuth(); // Utiliser le contexte d'authentification
+  const { pharmacy } = user;
 
-  const {pharmacy} = user
   return (
-    <AppBar position="fixed" sx={{ zIndex: 1201, backgroundColor: '#1F2A30',zIndex: (theme) => theme.zIndex.drawer + 1, }}>
+    <AppBar position="fixed" sx={{ zIndex: 1201, backgroundColor: '#1F2A30' }}>
       <Toolbar>
-        
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          {pharmacy.name}
+          {pharmacy?.name || 'Nom de la pharmacie'}
         </Typography>
+        
+        <Button
+          color="inherit"
+          onClick={logout} // Assurez-vous que la fonction logout est définie dans votre contexte
+          sx={{ ml: 2 }} // Ajout d'un espacement à gauche pour le bouton
+        >
+          Déconnexion
+        </Button>
       </Toolbar>
     </AppBar>
   );
